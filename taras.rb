3f4574bf86873ae784b5c0792e4975df1832988e
Taras Sveredyuk <taras.sveredyuk@gmail.com>
2016-08-22 21:10:01 +0300

    Weather.now

--- taras.rb
+++ taras.rb
@@ -1,8 +1,16 @@
-born_year = 1991
-now_year  = 2016
-age       = now_year / born_year
-adress    = 'Gorotska Street 187, Lviv City'
+# born_year = 1991
+# now_year  = 2016
+# age       = now_year / born_year
+# adress    = 'Gorotska Street 187, Lviv City'
 
-puts "Hello, Taras. I know that you are #{age} and you are living: #{adress}"
+# puts "Hello, Taras. I know that you are #{age} and you are living: #{adress}"
 
 
+class Weather
+	def now
+		'sunny'
+	end
+end
+
+today = Weather.new
+puts today.now
