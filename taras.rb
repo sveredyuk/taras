077a4224cc68905de617e032a40e7c3718658517
Volodya Sveredyuk <sveredyuk@gmail.com>
2016-08-22 20:54:08 +0300

    Better taras program

--- taras.rb
+++ taras.rb
@@ -1,1 +1,8 @@
-puts "Hello, Taras"
+born_year = 1991
+now_year  = 2016
+age       = now_year / born_year
+adress    = 'Gorotska Street 187, Lviv City'
+
+puts "Hello, Taras. I know that you are #{age} and you are living: #{adress}"
+
+
